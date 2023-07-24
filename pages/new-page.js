1f83917d5f7edce58f4758670082a744afc22eac
unknown <daveistomin@gmail.com>
2023-07-24 15:47:00 +0400

    initial commit

---
+++ pages/new-page.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+const newPage = () => {
+	return (
+		<div>
+			<h1>Это новая страница моего приложения</h1>
+			<p>Стас, саси хуй! Монс саси хуй! сасите хуй!</p>
+		</div>
+	);
+};
+
+export default newPage;
